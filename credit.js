55a763dba4079e41f9418d7a594ae96ca33c42d8
Khalid Mahmud <94211717+skhalidmahmud@users.noreply.github.com>
2025-07-16 05:39:39 +0600

    Create credit.js

---
+++ credit.js
@@ -0,0 +1,4 @@
+const devName = "Khalid Mahmud";
+const devURL = "https://skhalidmahmud.github.io";
+
+document.getElementById("dev-credit").innerHTML = `<a href="${devURL}" target="_blank">${devName}</a>`;
