1d145ac53f5304deccdba006f6c965a3d54df2ac
Khalid Mahmud <khalidfromdhaka@gmail.com>
2026-05-18 19:14:20 +0600

    Update developer URL to point to portfolio

--- credit.js
+++ credit.js
@@ -1,4 +1,4 @@
 const devName = "Md Khalid Mahmud";
-const devURL = "https://skhalidmahmud.github.io";
+const devURL = "https://skhalidmahmud.github.io/Portfolio/";
 
 document.getElementById("dev-credit").innerHTML = `<a href="${devURL}" target="_blank">${devName}</a>`;
